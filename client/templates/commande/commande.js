953ad7594aa2d268f8fd5a1533f7518680ca4234
Your Name <you@example.com>
2017-09-28 11:31:57 +0200

    Realise ajout de commentaires

--- client/templates/commande/commande.js
+++ client/templates/commande/commande.js
@@ -5,16 +5,19 @@ Meteor.subscribe("commandes");
 
 
 
-// Template.header.events({
-//     'submit .validercommande'() {
-//
-//         Panier.find().observe({
-//             Added:function (item) {
-//                  Commandes.insert(item);
-//           }
-//          });
-//     }
-//  });
+Template.header.events({
+    'submit .validercommande'() {
+
+        Panier.find().observe({
+            Added:function (item) {
+                 Commandes.insert(item);
+          }
+         });
+    }
+ });
+//ici on appelle l'evemement events du  template panier dans le template commandes pour
+//qu'à la soumission du bouton valider commande les données de la collection locales soient
+//transférées dans la collection commande coté serveur
 Template.panier.events({
     'submit .validercommande'(event) {
      event.preventDefault();
@@ -23,9 +26,13 @@ Template.panier.events({
                 Commandes.insert(item);
             }
         });
+        //on dirige les éléments ver la vue commande
         Router.go('commande')
     }
 });
+
+//on n'oublie pas d'appeler la fonctionalité du template commande qui retourne
+// tous les éléments de la collection commande.
 Template.commandes.helpers({
     commandes: function() {
          return Commandes.find();
@@ -33,6 +40,7 @@ Template.commandes.helpers({
 
 //  on rappelle la meme fonction qui sert au CALCUL du TOTAL PANIER dans
     //le helper du template commande car c'est un calcul qui se fait a la fin(total de la commande)
+    //car  c'est un calcul qui se fait avec les données contenues dans la collection concernée.
     'total': function () {
         // on initialise le
         total = 0;
